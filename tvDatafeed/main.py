cabd8d0a97b4de53b0dda7d401c1fc65da54ee5b
iblea <iblea@iasdf.com>
2026-04-08 17:51:19 +0900

    timeout time 변경

--- tvDatafeed/main.py
+++ tvDatafeed/main.py
@@ -31,7 +31,7 @@ class Interval(enum.Enum):
 class TvDatafeed:
     __search_url = 'https://symbol-search.tradingview.com/symbol_search/?text={}&hl=1&exchange={}&lang=en&type=&domain=production'
     __ws_headers = json.dumps({"Origin": "https://data.tradingview.com"})
-    __ws_timeout = 10
+    __ws_timeout = 20
 
     def __init__(
         self,
